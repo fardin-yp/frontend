c3e769ca2903b2a458c5b328f013c37d9d108203
mr-Alarak <82238611+fardin-yp@users.noreply.github.com>
2022-03-11 09:49:26 +0330

    Update NoPost.js

--- components/NoPost/NoPost.js
+++ components/NoPost/NoPost.js
@@ -5,7 +5,7 @@ const NoPost = ({name ,background}) => {
         <div>
             <div style={{marginBottom:"-300px" ,marginTop:"5px"}} className="err-card">
                <img src="/images/404.gif" alt=""  />
-               <p>متاسفانه هیچ وبسایت {name} برای فروش وجود ندارد!!</p>
+               <p style={{padding:"15px"}}>متاسفانه هیچ وبسایت {name} برای فروش وجود ندارد!!</p>
                <a style={{background:background}} href="/">بازگشت به صفحه اصلی</a>
             </div>
         </div>

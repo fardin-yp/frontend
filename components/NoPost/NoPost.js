a056fbc1e23be4949024a3fcf2c1fa6205f466d1
fardin-yp <fardin.werewolf0078@gmail.com>
2022-03-21 21:03:05 +0330

    first commit

--- components/NoPost/NoPost.js
+++ components/NoPost/NoPost.js
@@ -7,8 +7,6 @@ const NoPost = ({name ,background}) => {
                <img src="/images/404.gif" alt=""  />
                <p style={{padding:"15px"}}>متاسفانه هیچ وبسایت {name} برای فروش وجود ندارد!!</p>
 
-               <a href="/" style={{background:background}} >بازگشت به صفحه اصلی</a>
-
                <a href="/"  style={{background:background,zIndex:"20"}}>بازگشت به صفحه اصلی</a>
 
             </div>
